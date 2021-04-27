fbe08ac551e65bc4e0049f3d3436146f85ef3e0a
Nuno Marques <nuno_marques__1@hotmail.com>
2021-04-27 11:01:00 +0100

    New

---
+++ proj.java
@@ -0,0 +1,20 @@
+/*
+ * Nome: Nuno Marques
+ * Nº: 38958
+ * 
+ * Nome: Tomás Vicente
+ * Nº: 39067
+ * 
+ * Compile:
+ * javac -encoding utf8 proj.java
+ * 
+ * Execute:
+ * java -Xmx64M -Xss32M -classpath . proj
+ */
+public class proj {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
